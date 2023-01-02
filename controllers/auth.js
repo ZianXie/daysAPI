ec8c7c62cdff57b4da7715b972e3839133dc7652
VS Code Windows <ansontse17@gmail.com>
2023-01-01 23:43:57 -0500

    sign a jwt token in register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 
 const register = async (req, res) => {
-
+    console.log('hit register middleware...');
     const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json(req.body)
+    const token = jwt.sign({ mongoDocID: user._id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '30d' })
+    res.status(StatusCodes.CREATED).json({ username: user.name, token })
 }
 
 const login = async (req, res) => {

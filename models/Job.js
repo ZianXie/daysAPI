26b7689a99441417d57d0edbe52d05e47e9961f5
VS Code Windows <ansontse17@gmail.com>
2023-01-02 12:46:20 -0500

    jobs schema

---
+++ models/Job.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const JobSchema = new mongoose.Schema(
+    {
+        company: {
+            type: String,
+            required: [true, 'no company name provided!!!'],
+            maxlength: 50
+        },
+        position: {
+            type: String,
+            required: [true, 'No position provided!!!'],
+            maxlength: 100
+        },
+        status: {
+            type: String,
+            enum: ['interview', 'declined', 'pending'],
+            default: 'pending'
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'a user is needed for this doc'],
+        }
+    }, { timestamps: true }
+)
+
+export default JobSchema
